90c5b67a66a3af7cc33f47757950bbf1055125ef
cfalcant <32115349+cfalcant@users.noreply.github.com>
2017-11-01 14:20:36 -0700

    Updated JS
    Updated JS

--- map.js
+++ map.js
@@ -1,15 +1,4 @@
-mapboxgl.accessToken = "pk.eyJ1IjoiY2ZhbGNhbnQiLCJhIjoiY2o5Z2UxaHRkMngzaDMzbnJ5MDd6ZmxnMiJ9.qadx8NPsZFFFlsI7cCLwqg";
-// This adds the map to your page
-var map = new mapboxgl.Map({
-  // container id specified in the HTML
-  container: 'map',
-  // style URL
-  style: 'mapbox://styles/mapbox/light-v9'
-  // initial position in [lon, lat] format
-  center: [-77.034084, 38.909671],
-  // initial zoom
-  zoom: 14
-});
+
 var stores = {
   //data from sweetgreen.geojson, downloaded above
 };
